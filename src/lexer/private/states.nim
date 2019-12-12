30562ea700c470183c927d4dd09e743e79232e63
Michael Caley <mjcaley@darkarctic.com>
2019-12-11 19:57:16 -0500

    Started passing Lexer state tests

--- src/lexer/private/states.nim
+++ src/lexer/private/states.nim
@@ -1,32 +1,29 @@
 import options
 import ../position, ../token, ../../utility/stack
 import lexer_object, constants, context, utility
+export position, token
 
 
 proc transition*(self: var Lexer, state: State) =
     self.state = state
   
 proc startState*(self: var Lexer) : Option[Token] =
-  debugEcho "[Start] Enter"
   discard self.context.advance()
   discard self.context.advance()
   self.context.indents.push(0)
   transition self, State.IsEOF
-  # result = some(initToken(Indent))
-  debugEcho "[Start] Exit"
+  result = some(initToken(Indent))
 
 proc isEOFState*(self: var Lexer) : Option[Token] =
-  debugEcho "[IsEOF] Enter"
   if self.eof:
     transition self, State.Dedent
   else:
     transition self, State.Indent
-  debugEcho "[IsEOF] Exit"
 
 proc indentState(self: var Lexer) : Option[Token] =
   debugEcho "[Indent] Enter"
   let position = self.context.currentPosition
-  echo "indent position ", position
+  # echo "indent position ", position
 
   if position.column != 1:
     skipWhitespace self
@@ -63,42 +60,49 @@ debugEcho "[Indent] Exit"
 
 proc dedentState(self: var Lexer) : Option[Token] =
   debugEcho "[Dedent] Enter"
+  if self.context.indents.empty:
+    transition self, End
+    return none Token
+
   let target = self.lexeme.len
   if target < self.context.indents.top:
     discard self.context.indents.pop
     transition self, State.IsEOF
     result = some initToken(Dedent)
-  elif target == self.context.indents.top:
+  elif target == self.context.indents.top and not self.eof:
     transition self, State.Operator
+  elif target == self.context.indents.top and self.eof:
+    discard self.context.indents.pop
+    transition self, State.End
+    result = some initToken(Dedent)
   else:
     transition self, End
-  # let targetDedent = self.lexeme.len
-  # if self.context.indents.top > targetDedent:
-  #   discard self.context.indents.pop()
-  #   transition self, State.IsEOF
-  #   result = some initToken(Dedent)
-  # elif self.context.indents.top == 0 and self.eof:
-  #   discard self.consume()
-  #   transition self, State.End
 
   debugEcho "[Dedent] Exit"
 
 proc operatorState(self: var Lexer) : Option[Token] =
   debugEcho "[Operator] Enter"
+  self.skipWhitespace()
+
   let position = self.context.currentPosition
 
+  debugEcho "Current: ", self.context.currentCharacter, " Next: ", self.context.nextCharacter
   case self.current:
     of "\n":
+      debugEcho "is it newline?"
       discard self.context.advance()
       result = some initToken(Newline, position)
       transition self, IsEOF
     of "+":
+      debugEcho "is it plus?"
       discard self.context.advance()
       result = some initToken(Plus, position)
       transition self, IsEOF
     of "0", "1", "2", "3", "4", "5", "6", "7", "9":
+      debugEcho "is it a number?"
       transition self, State.Number
     else:
+      debugEcho "no match, go to IsEOF"
       transition self, IsEOF
 
   # if self.matchAny(DigitChars):
